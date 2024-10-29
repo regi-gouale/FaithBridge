2722a36ea71a35420558e4e5dd817f717033b3b5
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2024-10-29 19:28:22 +0100

    Update README.md and add font to auth-frontend/app/page.tsx (#1)

---
+++ auth-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <h1 className="my-4 font-heading text-2xl font-bold uppercase">Authentification</h1>
+    </div>
+  );
+}
