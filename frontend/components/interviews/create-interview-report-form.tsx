e1ba017a3fa0b263760e04b613423ede8dd74f04
Régi GOUALE <regi.gouale@gmail.com>
2024-11-07 09:03:11 +0100

    Ajout d'un formulaire pour créer un rapport d'entretien et d'un hook associé

---
+++ frontend/components/interviews/create-interview-report-form.tsx
@@ -0,0 +1,19 @@
+import {} from "@hookform/resolvers/zod";
+import {} from "react";
+import {} from "react-hook-form";
+import {} from "zod";
+
+import {} from "@/components/ui/button";
+import {} from "@/components/ui/dialog";
+import {} from "@/components/ui/form";
+import {} from "@/components/ui/input";
+
+interface CreateInterviewReportFormProps {
+  onCancel?: () => void;
+}
+
+export const CreateInterviewReportForm = ({
+  onCancel,
+}: CreateInterviewReportFormProps) => {
+  return <div>CreateInterviewReportForm</div>;
+};
